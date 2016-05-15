fcf0a540e2128127b8c7ea4d68123120b06d01c0
mandy23832 <mandy23832@gmail.com>
2016-05-15 21:55:24 +0800

    Create GamePlay.h

---
+++ GamePlay.h
@@ -0,0 +1,12 @@
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+
+#include <QGraphicsRectItem>
+#include<QObject>
+
+class GamePlay:public QGraphicsRectItem{
+public:
+    void create();
+};
+
+#endif // GAMEPLAY_H
