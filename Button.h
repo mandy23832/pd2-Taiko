647d568f5cf9554a257cb54b380f81d55a4da3ac
mandy23832 <mandy23832@gmail.com>
2016-05-15 21:54:39 +0800

    Create Button.h

---
+++ Button.h
@@ -0,0 +1,24 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include<QGraphicsRectItem>
+#include<QGraphicsSceneMoveEvent>
+#include<QString>
+
+class  Button:public QObject,public QGraphicsRectItem{
+    Q_OBJECT
+public:
+    //constructor
+    Button(QString name, QGraphicsItem* parent=NULL);
+
+    //public methods(events)
+    void mousePressEvent(QGraphicsSceneMouseEvent * event);
+    void hoverEnterEvent(QGraphicsSceneHoverEvent * event);
+    void hoverLeaveEvent(QGraphicsSceneHoverEvent * event);
+signals:
+    void clicked();
+private:
+    QString text;
+
+};
+#endif // BUTTON_H
